cde654b7a7d374bd1a6fc3fced5772b9c42a2d85
anassoomro1 <153824882+anassoomro1@users.noreply.github.com>
2024-06-01 17:23:31 -0700

    final
    final

--- app.js
+++ app.js
@@ -4,3 +4,20 @@
 
 const div = document.querySelector('div')
 
+
+axios('https://fakestoreapi.com/products')
+.then((res) => {
+    console.log(res.data); 
+    res.data.map((item)=>{
+        div.innerHTML+= `
+        <h1>category: ${item.category} </h1>
+        <h1>title: ${item.title} </h1>
+        <img width= "300" src="${item.title}" alt="item.image">
+        <h1>price: ${item.price} </h1>
+        <hr/>
+        `
+    })
+})
+  .catch((error) => {
+    console.error(error); 
+  });
