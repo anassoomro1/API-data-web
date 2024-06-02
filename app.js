553a2f0790a3267d0b71afab312ccd4b87fcf244
anassoomro1 <153824882+anassoomro1@users.noreply.github.com>
2024-06-01 17:43:07 -0700

    some
    changing

--- app.js
+++ app.js
@@ -5,14 +5,14 @@
 const div = document.querySelector('div')
 
 
-axios('https://fakestoreapi.com/products')
+axios('https://api.escuelajs.co/api/v1/products')
 .then((res) => {
     console.log(res.data); 
     res.data.map((item)=>{
         div.innerHTML+= `
         <h1>category: ${item.category} </h1>
         <h1>title: ${item.title} </h1>
-        <img width= "300" src="${item.title}" alt="item.image">
+        <img src="${item.image}" alt="image-products"/>
         <h1>price: ${item.price} </h1>
         <hr/>
         `
